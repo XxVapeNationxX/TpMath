5873e73c00aa1d635af148beaeb932884fcbb9be
Charles Bourgeois <charlesbourgeois@live.ca>
2019-04-25 22:44:10 -0400

    dd

--- TpMath/TP_Math/TP_Math/Intérieur.Designer.cs
+++ TpMath/TP_Math/TP_Math/Intérieur.Designer.cs
@@ -40,6 +40,12 @@
             this.label4 = new System.Windows.Forms.Label();
             this.BTN_Return = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.BTN_Return)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -85,7 +91,7 @@
             // BTN_Calculer
             // 
             this.BTN_Calculer.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_Calculer.Location = new System.Drawing.Point(150, 122);
+            this.BTN_Calculer.Location = new System.Drawing.Point(150, 224);
             this.BTN_Calculer.Name = "BTN_Calculer";
             this.BTN_Calculer.Size = new System.Drawing.Size(101, 26);
             this.BTN_Calculer.TabIndex = 5;
@@ -121,7 +127,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Location = new System.Drawing.Point(19, 271);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 22);
             this.label3.TabIndex = 8;
@@ -131,7 +137,7 @@
             // 
             this.TBX_Reponse.Enabled = false;
             this.TBX_Reponse.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TBX_Reponse.Location = new System.Drawing.Point(95, 162);
+            this.TBX_Reponse.Location = new System.Drawing.Point(102, 272);
             this.TBX_Reponse.Name = "TBX_Reponse";
             this.TBX_Reponse.Size = new System.Drawing.Size(149, 24);
             this.TBX_Reponse.TabIndex = 9;
@@ -139,7 +145,7 @@
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(272, 161);
+            this.label4.Location = new System.Drawing.Point(272, 224);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label4.Size = new System.Drawing.Size(218, 67);
@@ -149,7 +155,7 @@
             // BTN_Return
             // 
             this.BTN_Return.Image = global::TP_Math.Properties.Resources._61449_return_button;
-            this.BTN_Return.Location = new System.Drawing.Point(8, 197);
+            this.BTN_Return.Location = new System.Drawing.Point(11, 310);
             this.BTN_Return.Name = "BTN_Return";
             this.BTN_Return.Size = new System.Drawing.Size(26, 23);
             this.BTN_Return.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -162,16 +168,82 @@
             this.pictureBox1.Image = global::TP_Math.Properties.Resources.probability_density_function;
             this.pictureBox1.Location = new System.Drawing.Point(276, 18);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(214, 130);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 188);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(102, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 24);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(99, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Champ non valide";
+            this.label5.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(102, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 24);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(99, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Champ non valide";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(70, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 22);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "A:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(71, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 22);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "B:";
             // 
             // Intérieur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 228);
+            this.ClientSize = new System.Drawing.Size(502, 345);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.BTN_Return);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.TBX_Reponse);
@@ -185,7 +257,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Intérieur";
-            this.Text = "Intérieur";
+            this.Text = "c";
             ((System.ComponentModel.ISupportInitialize)(this.BTN_Return)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -207,5 +279,11 @@
         private System.Windows.Forms.TextBox TBX_Reponse;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox BTN_Return;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
