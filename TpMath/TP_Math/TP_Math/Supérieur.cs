e85399401bc0ca5bf09a1844a01b1251d78491f2
Charles Bourgeois <charlesbourgeois@live.ca>
2019-04-25 21:19:19 -0400

    df

--- TpMath/TP_Math/TP_Math/Supérieur.cs
+++ TpMath/TP_Math/TP_Math/Supérieur.cs
@@ -27,7 +27,7 @@ namespace TP_Math
         private void TBX_Moyenne_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
-       (e.KeyChar != '.'))
+       (e.KeyChar != '.') && (e.KeyChar != '-'))
             {
                 e.Handled = true;
             }
@@ -38,7 +38,7 @@ namespace TP_Math
                 e.Handled = true;
             }
             // only allow one decimal point
-            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
             {
                 e.Handled = true;
             }
